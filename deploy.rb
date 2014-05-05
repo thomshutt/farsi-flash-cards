ec3afc9c96631f8624b9b9f7e81123d613c30bb1
Thom Shutt <thom@thomshutt.com>
2014-05-05 17:51:49 +0100

    Added command for committing latest built version

--- deploy.rb
+++ deploy.rb
@@ -14,6 +14,8 @@
   end
   system "git checkout gh-pages"
   system "./build"
+  system "git add farsi-flash-cards.zip"
+  system "git commit -m \"Added latest packaged version\""
   system "git merge master -m 'Updating Github Pages branch'"
   system "git push origin gh-pages"
   system "git checkout master"
